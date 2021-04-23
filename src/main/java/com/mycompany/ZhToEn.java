bdbf62e3e0345beb1f33fa4500efd53606c2cecf
dendendendendeng <1471720721@qq.com>
2021-04-23 19:26:07 +0800

    增加list鼠标监听事件，实现翻译后的字符替换

--- src/main/java/com/mycompany/ZhToEn.java
+++ src/main/java/com/mycompany/ZhToEn.java
@@ -4,10 +4,12 @@ import com.alibaba.fastjson.JSON;import com.intellij.ide.util.PropertiesComponen
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.SelectionModel;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.ui.popup.*;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.ui.JBColor;
 import com.intellij.ui.components.JBList;
 import com.mycompany.Util.Utils;
@@ -18,6 +20,10 @@ import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
@@ -39,35 +45,30 @@ public class ZhToEn extends AnAction {
         //获取选中的文本 
         SelectionModel model = mEditor.getSelectionModel();
         String selectedText = null;
-        try {
-            System.out.println(new String(model.getSelectedText().getBytes("utf-8"),"utf-8"));
-            selectedText = new String(model.getSelectedText().getBytes("utf-8"),"utf-8");
-            if(model.getSelectedText().matches("[\u4E00-\u9FA5]+"))
-                isZhToEn = true;
-            else isZhToEn = false;
-            System.out.println("初始的selectedText字节数为:"+selectedText.getBytes("utf-8").length+selectedText);
-        } catch (UnsupportedEncodingException unsupportedEncodingException) {
-            unsupportedEncodingException.printStackTrace();
-        }
+        selectedText = model.getSelectedText();
+        if(model.getSelectedText().matches(".*[\u4E00-\u9FA5].*"))
+            isZhToEn = true;
+        else isZhToEn = false;
         if (TextUtils.isEmpty(selectedText)) {
             return;
         }
 
+        System.out.println(selectedText);
         PropertiesComponent propertiesComponent = PropertiesComponent.getInstance();
+        TextRange textRange = new TextRange(model.getSelectionStart(),model.getSelectionEnd());
         if(propertiesComponent.getValue(selectedText) != null) {//已经查询过一次
+            JBPopupFactory factory = JBPopupFactory.getInstance();
             if(isZhToEn){
                 String[] translationArray = propertiesComponent.getValues(selectedText);
-                zhToEn(translationArray);
-                return;
+                zhToEn(translationArray,e,textRange);
             }else{
                 String lastResult = propertiesComponent.getValue(selectedText);
-                JBPopupFactory factory = JBPopupFactory.getInstance();
                 factory.createHtmlTextBalloonBuilder(lastResult, null, new JBColor(new Color(186, 238, 186), new Color(73, 117, 73)), null)
                         .setFadeoutTime(100000)
                         .createBalloon()
                         .show(factory.guessBestPopupLocation(mEditor), Balloon.Position.below);
-                return;
             }
+            return;
         }
         //post方式发送查询数据
         String result = "";
@@ -90,8 +91,6 @@ public class ZhToEn extends AnAction {
             String sign = Utils.SHA256(signStr);
 
             StringBuffer buffer = new StringBuffer();
-            String s = "q="+selectedText;
-            System.out.println("q=+selectedText 的长度为"+s.getBytes().length);
             buffer.append("q="+selectedText);
             buffer.append("&from=auto");
             buffer.append("&appKey="+YOUDAO_APP_KEY);
@@ -101,6 +100,8 @@ public class ZhToEn extends AnAction {
             buffer.append("&curtime="+curTime);
             if(isZhToEn) buffer.append("&to=en");
             else buffer.append("&to=zh-CHS");
+
+            System.out.println("请求参数为："+buffer.toString());
 
             connection.setDoOutput(true);
             connection.setDoInput(true);
@@ -125,121 +126,119 @@ public class ZhToEn extends AnAction {
             }
         }
 
-        try{
-            if(result.equals("")){
-                Messages.showMessageDialog(new String("请求数据失败了 ".getBytes(),"utf-8"), "Translation", Messages.getInformationIcon());
+        if(result.equals("")){
+            Messages.showMessageDialog("请求数据失败了 ", "Translation", Messages.getInformationIcon());
+        }else{
+            System.out.println("请求结果为"+result);
+            YouDaoResult youDaoResult = JSON.parseObject(result, YouDaoResult.class);
+            if (isZhToEn){
+                String[] translationArray = stringListToStringArray(youDaoResult.translation);
+                zhToEn(translationArray,e,textRange);
+                propertiesComponent.setValues(youDaoResult.query,translationArray);
             }else{
-                System.out.println("请求结果为"+result);
-                YouDaoResult youDaoResult = JSON.parseObject(result, YouDaoResult.class);
-                if (isZhToEn){
-                    String[] translationArray = stringListToStringArray(youDaoResult.translation);
-
-                    JBList<String> list = new JBList<>();
-                    list.setListData(translationArray);
-                    list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);// 允许可间断的多选// 添加选项选中状态被改变的监听器
-                    list.addListSelectionListener(new ListSelectionListener() {
-                        @Override
-                        public void valueChanged(ListSelectionEvent e) {
-                            int[] indices = list.getSelectedIndices();// 获取所有被选中的选项索引
-                            ListModel<String> listModel = list.getModel();// 获取选项数据的 ListModel
-                            // 输出选中的选项
-                            for (int index : indices) {
-                                //todo 获取最后一个选中的翻译结果，填回源代码
-                                selectResult = listModel.getElementAt(index);
-                                System.out.println("选中: " + index + " = " + listModel.getElementAt(index));
-                            }
-                            System.out.println();
-                        }
-                    });
-                    list.setSelectedIndex(0);// 设置默认选中项
-                    JBPopupFactory.getInstance().createComponentPopupBuilder(list,null).createPopup().showInBestPositionFor(e.getDataContext());
-//                    ListPopup listPopup = (ListPopup) JBPopupFactory.getInstance().createPopupChooserBuilder(youDaoResult.translation);
-//                    listPopup.showInBestPositionFor(e.getDataContext());
-
-//                    if(translationArray != null){
-//                        zhToEn(translationArray);
-//                        propertiesComponent.setValues(youDaoResult.query,translationArray);//保存查询后的英文数组
-//                        System.out.println(result);
-//                    }else {
-//                        Messages.showMessageDialog(new String("请求数据失败 结果为空".getBytes(),"utf-8"), "Translation", Messages.getInformationIcon());
-//                    }
-                }else{
-                    enToZh(selectedText,youDaoResult,mEditor);
-                }
+                enToZh(selectedText,youDaoResult,mEditor);
             }
-        }catch (UnsupportedEncodingException unsupportedEncodingException) {
-            unsupportedEncodingException.printStackTrace();
         }
     }
 
     //显示英译中的结果
     public void enToZh(String selectedText,YouDaoResult youDaoResult,Editor mEditor){
-        try {
-            PropertiesComponent propertiesComponent = PropertiesComponent.getInstance();//持久化保存数据
-            StringBuffer zhResult = new StringBuffer();//TODO
-            zhResult.append(new String("选中的文本为:".getBytes(), "utf-8"));
-            zhResult.append(new String(selectedText.getBytes(), "utf-8"));
-            zhResult.append(new String("\n\n基本翻译为：\n".getBytes(), "utf-8"));
-            for (String item : youDaoResult.translation) {
-                zhResult.append(item + "\n");
-            }
-            if (youDaoResult.web != null) {
-                zhResult.append(new String("\n网络释义为：\n".getBytes(), "utf-8"));
-                for (WebPart part : youDaoResult.web) {
-                    zhResult.append(part.key + ":");
-                    for (int webIndex = 0; webIndex < part.value.size(); webIndex++) {
-                        if (webIndex != 0) zhResult.append("," + part.value.get(webIndex));
-                        else zhResult.append(part.value.get(webIndex));
-                    }
-                    zhResult.append("\n");
-                }
-            }
-            propertiesComponent.setValue(youDaoResult.query,zhResult.toString());//将查询到的英译中处理后保存起来
-            JBPopupFactory factory = JBPopupFactory.getInstance();
-            factory.createHtmlTextBalloonBuilder(zhResult.toString(), null, new JBColor(new Color(186, 238, 186), new Color(73, 117, 73)), null)
-                    .setFadeoutTime(100000)
-                    .createBalloon()
-                    .show(factory.guessBestPopupLocation(mEditor), Balloon.Position.below);
-        }catch (UnsupportedEncodingException unsupportedEncodingException) {
-            unsupportedEncodingException.printStackTrace();
+        PropertiesComponent propertiesComponent = PropertiesComponent.getInstance();//持久化保存数据
+        StringBuffer zhResult = new StringBuffer();
+        zhResult.append("选中的文本为:");
+        zhResult.append(selectedText);
+        zhResult.append("\n\n基本翻译为：\n");
+        if(youDaoResult.translation == null) {
+            Messages.showMessageDialog("translation字段为空", "Translation", Messages.getInformationIcon());
+            return ;
         }
+        for (String item : youDaoResult.translation) {
+            zhResult.append(item + "\n");
+        }
+        if (youDaoResult.web != null) {
+            zhResult.append("\n网络释义为：\n");
+            for (WebPart part : youDaoResult.web) {
+                zhResult.append(part.key + ":");
+                for (int webIndex = 0; webIndex < part.value.size(); webIndex++) {
+                    if (webIndex != 0) zhResult.append("," + part.value.get(webIndex));
+                    else zhResult.append(part.value.get(webIndex));
+                }
+                zhResult.append("\n");
+            }
+        }
+        propertiesComponent.setValue(youDaoResult.query,zhResult.toString());//将查询到的英译中处理后保存起来
+        JBPopupFactory factory = JBPopupFactory.getInstance();
+        factory.createHtmlTextBalloonBuilder(zhResult.toString(), null, new JBColor(new Color(186, 238, 186), new Color(73, 117, 73)), null)
+                .setFadeoutTime(100000)
+                .createBalloon()
+                .show(factory.guessBestPopupLocation(mEditor), Balloon.Position.below);
     }
 
     //显示中译英的结果，可以选中并替换
-    public void zhToEn(String[] results){
+    public void zhToEn(String[] results,AnActionEvent event,TextRange textRange){
+        Editor editor = event.getData(PlatformDataKeys.EDITOR);
+        SelectionModel model = editor.getSelectionModel();
+        JBList<String> list = new JBList<>();
+        String[] tests = new String[]{"hello","hi","apple"};
+        list.setListData(tests);
+        list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);// 允许可间断的多选// 添加选项选中状态被改变的监听器
 
-        JFrame jf = new JFrame("Translation");
-        JPanel panel = new JPanel();
+        JBPopup popup = JBPopupFactory.getInstance().createComponentPopupBuilder(list,null).createPopup();
 
-        final JBList<String> list = new JBList<>();// 创建一个 JList 实例
-        list.setPreferredSize(new Dimension(200, 100));// 设置一下首选大小
-        list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);// 允许可间断的多选
-
-        list.setListData(results);// 设置选项数据（内部将自动封装成 ListModel ）
-
-        // 添加选项选中状态被改变的监听器
-        list.addListSelectionListener(new ListSelectionListener() {
+        list.addListSelectionListener(e -> {
+            int index = list.getLeadSelectionIndex();//
+            ListModel<String> listModel = list.getModel();// 获取选项数据的 ListModel
+            // 输出选中的选项
+            selectResult = listModel.getElementAt(index);
+            System.out.println("选中: " + index + " = " + listModel.getElementAt(index));
+        });
+        list.addMouseListener(new MouseListener() {
             @Override
-            public void valueChanged(ListSelectionEvent e) {
-                int[] indices = list.getSelectedIndices();// 获取所有被选中的选项索引
-                ListModel<String> listModel = list.getModel();// 获取选项数据的 ListModel
-                // 输出选中的选项
-                for (int index : indices) {
-                    //todo 获取最后一个选中的翻译结果，填回源代码
-                    selectResult = listModel.getElementAt(index);
-                    System.out.println("选中: " + index + " = " + listModel.getElementAt(index));
+            public void mouseClicked(MouseEvent e) {
+                if(e.getClickCount() == 3){
+                    WriteCommandAction.runWriteCommandAction(event.getProject(), () -> editor.getDocument().replaceString(textRange.getStartOffset(),textRange.getEndOffset(),selectResult));
+                    model.removeSelection();//移除选中项
+                    popup.cancel();//隐藏列表项
                 }
-                System.out.println();
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+            }
+        });
+        list.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {//回车监听事件还是没法响应
+                if(e.getKeyCode() == KeyEvent.VK_CONTROL){
+                    WriteCommandAction.runWriteCommandAction(event.getProject(), () -> editor.getDocument().replaceString(textRange.getStartOffset(),textRange.getEndOffset(),selectResult));
+                    model.removeSelection();//移除选中项
+                    popup.cancel();//隐藏列表项
+                }
+                
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
             }
         });
 
         list.setSelectedIndex(0);// 设置默认选中项
-        panel.add(list);// 添加到内容面板容器
-        jf.setContentPane(panel);
-        jf.setVisible(true);//设置可见
-        jf.setSize(300, 300);
-        jf.setLocationRelativeTo(null);
-        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        jf.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);//设置关闭时的操作
+        popup.showInBestPositionFor(editor);
     }
 }
